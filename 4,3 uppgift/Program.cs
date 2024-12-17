db864720abe9a06cda9f15b8809db82445444104
ryhu06001 <ryhu06001@E-PW05CGRC>
2024-12-17 14:30:24 +0100

    Add project files.

---
+++ 4,3 uppgift/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _4_3_uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int summa = 0; 
+           
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write($"Ange heltal {i}: "); 
+                int tal;
+
+                
+                while (!int.TryParse(Console.ReadLine(), out tal))
+                {
+                    Console.Write("Ogiltig inmatning. Ange ett heltal: ");
+                }
+
+                summa += tal;
+            }
+
+           
+            Console.WriteLine($"Summan av de inmatade talen är: {summa}");
+        }
+    }
+}
